0b10c0127c61ef0a112d73675ba80d598a5dde54
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-18 21:18:28 +0530

    dd

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hi");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+const PORT = 4000;
+const fs = require("fs");
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3048",
+  },
+});
+const savedData = fs.readFileSync("data.json");
+const objectData = JSON.parse(savedData);
+
+// console.log(savedData, "savedData");
+// console.log(objectData, "objectData");
+
+//route
+app.get("/api", (req, res) => {
+  const data = fs.readFileSync("data.json");
+  const products = JSON.parse(data);
+  res.json(products);
+});
+//
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
