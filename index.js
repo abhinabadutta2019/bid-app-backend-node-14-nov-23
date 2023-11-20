d358898ea53ebe45b143ebdf743fd3e3de806c4a
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-21 02:49:14 +0530

    dd

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const socketIO = require("socket.io")(http, {
 // Enable CORS for all routes
 app.use(cors());
 //
+// Enable JSON parsing middleware
+app.use(express.json());
+//
 const savedData = fs.readFileSync("data.json");
 const objectData = JSON.parse(savedData);
 //
@@ -38,6 +41,43 @@ app.get("/api", (req, res) => {
   const products = JSON.parse(data);
   res.json(products);
 });
+//
+// Add a new product route
+app.post("/api/add-product", (req, res) => {
+  try {
+    const data = fs.readFileSync("data.json");
+    let products = JSON.parse(data);
+
+    // Make sure products is an array, initialize as an empty array if not
+    if (!Array.isArray(products)) {
+      products = [];
+    }
+
+    const newProduct = {
+      id: products.length + 1,
+      name: req.body.name,
+      price: req.body.price,
+      owner: req.body.owner,
+      last_bidder: req.body.last_bidder,
+    };
+
+    // Add the new product to the existing array
+    products.push(newProduct);
+
+    fs.writeFileSync("data.json", JSON.stringify(products, null, 2));
+
+    // Notify connected clients about the new product
+    socketIO.emit("newProductAdded", newProduct);
+
+    res
+      .status(201)
+      .json({ message: "Product added successfully", product: newProduct });
+  } catch (error) {
+    console.error("Error adding product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 //
 
 http.listen(PORT, () => {
