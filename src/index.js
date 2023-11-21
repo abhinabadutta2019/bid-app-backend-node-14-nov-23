699f8a0eab789cf0772b3c57f19f2030af05e61d
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-21 17:36:27 +0530

    made changes getting error

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { productRouter } from "./routers/user";
+const app = express();
+const http = require("http").Server(app);
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3048",
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+  },
+});
+//
+// Enable CORS for all routes
+app.use(cors());
+//
+// Enable JSON parsing middleware
+app.use(express.json());
+//
+dotenv.config();
+//
+const savedData = fs.readFileSync("data.json");
+const objectData = JSON.parse(savedData);
+//
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  //
+  // Emit a custom event to notify the frontend about the new user
+  socketIO.emit("userConnected", socket.id);
+  //
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    //
+    // Emit a custom event to notify the frontend about the disconnected user
+    socketIO.emit("userDisconnected", socket.id);
+  });
+});
+//
+///////mongoDB cloud//////////////////
+let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.te788iv.mongodb.net/assign-socket-io-bid-app-nodejs-nov-23?retryWrites=true&w=majority`;
+//
+async function connectToMongoDB() {
+  try {
+    //if mongoDB uri is correct
+    //if it is connected
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    //if error in connection or - in mongoDB uri
+    console.error("MongoDB connection error:", error);
+  }
+}
+// Call the async function to connect to MongoDB
+connectToMongoDB();
+////////////////////////////////////////////
+//
+
+//
+//
+// routes
+app.use("/product", productRouter);
+//
+const PORT = process.env.PORT || 4000;
+//
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
